424d3048d4568e716b58638810565af50a9cbd4d
jhemi-cristina <jhemilycristina@gmail.com>
2022-06-21 13:54:12 -0300

    feat: deleting note

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -44,6 +44,13 @@ class NotesController {
 
     return response.json({ ...note, tags, links });
   }
+
+  async delete(request, response) {
+    const { id } = request.params;
+    await knex("notes").where({ id }).delete();
+
+    return response.json();
+  }
 }
 
 module.exports = NotesController;
