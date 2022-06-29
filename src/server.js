aa2c620ae77f00d0aa370945a35e2a8f67348414
jhemi-cristina <jhemilycristina@gmail.com>
2022-06-29 14:10:34 -0300

    feat: serving files

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
+const uploadConfig = require("./configs/upload");
 
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 migrationsRun();
 
