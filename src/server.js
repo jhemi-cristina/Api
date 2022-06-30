87e1f83e8ae05ba452a73e12dd88cde11cc23dd6
jhemi-cristina <jhemilycristina@gmail.com>
2022-06-30 13:56:21 -0300

    feat: implemet cors project

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const uploadConfig = require("./configs/upload");
+const cors = require("cors");
 
 const routes = require("./routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
