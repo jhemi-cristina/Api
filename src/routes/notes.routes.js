36444d974aa9e5d81b3aa597668ea97c8ed6df56
jhemi-cristina <jhemilycristina@gmail.com>
2022-07-07 16:05:40 -0300

    resolved bugs

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -9,7 +9,7 @@ const notesController = new NotesController();
 notesRoutes.use(ensureAuthenticated);
 
 notesRoutes.post("/", notesController.create);
-notesRoutes.get("/", notesController.index);
+notesRoutes.get("/:id", notesController.index);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
 
